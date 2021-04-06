710a01f4caea826671faf4123410e661f8d46f1b
flvSantos15 <48564704+flvSantos15@users.noreply.github.com>
2021-04-06 17:14:57 -0300

    README.md
    #URNA ELETRONICA
    
    >>Explicação
    Aplicação web que simula o uso de uma urna eletronica, que consome um arquivo json com alguns candidatos.
    
    >>Como usar:
    Digite o número dos seus candidatos, se estiver tudo ok, confirme, se não corrija e senão quiser votar apenas anule.
    
    >>Tecnologias:
    html, css e javascript
    
    >>Sobre:
    Essa é a primeira versão, prentendo aprimorar a aplicação, utlizando o reactjs e outras ferramentas.

---
+++ js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+   {
+      titulo: 'VEREADOR',
+      numeros: 5,
+      candidados: [
+         {
+            numero: '38111',
+            nome: 'FULANO DE TAL',
+            partido: 'ABC',
+            foto:[
+               //{url:'38111.jpg', legenda: 'Vereador', small: false}
+            ]
+         },
+         {
+            numero: '77222',
+            nome: 'Beltrano da Silva',
+            partido: 'DEFG',
+            foto:[
+               //{url:'77222.jpg', legenda: 'Vereador'}
+            ]
+         },
+      ]
+   },
+   {
+      titulo: 'PREFEITO',
+      numeros: 2,
+      candidados: [
+         {
+            numero: '99',
+            nome: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Cisc',
+            fotos: [
+               //{url: '99.jpg', legenda: 'Prefeito'},
+               //{url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+            ]
+         },
+         {
+            numero: '84',
+            nome: 'Zulano',
+            partido: 'QWERTY',
+            vice: 'Zul',
+            fotos: [
+               //{url: '84.jpg', legenda: 'Prefeito'},
+               //{url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+            ]
+         },
+      ]
+   }
+]
